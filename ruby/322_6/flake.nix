3b03c9d57494c151dc105f045fbe67097c6daf9b
omusamurai <9350722+Sylvance@users.noreply.github.com>
2023-09-09 23:37:14 +0300

    [flakes] Update to use bundler

--- ruby/322_6/flake.nix
+++ ruby/322_6/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A flake for Ruby 3.2.2 and Rails 6 projects.";
+  description = "A flake for Ruby 3.2.2, Bundler 2.4.13 and Rails 6 projects.";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
@@ -18,11 +18,15 @@
       # Define Rails 6
       rails6 = nixpkgs.pkgs.rubyPackages.rails_6;
 
+      # Define Bundler 2.4.13
+      bundler = nixpkgs.pkgs.rubyPackages.bundler_2_4_13;
+
     in
     {
       packages = {
         ruby322 = ruby322;
         rails6 = rails6;
+        bundler = bundler;
       };
     };
 }
